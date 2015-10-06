b1452eaca2cae888ba1786694badccd7c62d6b62
Henry Miskin <henry.miskin@talktalk.net>
2015-10-06 21:47:11 +0100

    Changed functionality of scratchblock creation

--- main/app.js
+++ main/app.js
@@ -4,15 +4,31 @@ angular.module('angularScratchblocks',[]).directive('scratchblock',function(){
 
 	function link(scope, element, attrs){
 
-    angular.element(element).ready(function () {
-			scratchblocks2.parse('pre.scratchblocks');
-    });
+    var $el = $(element);
+    var $container = $('<div>');
+
+    var code = scope.code.replace(/<br>\s?|\n|\r\n|\r/ig, '\n');
+    var scripts = scratchblocks2.parse_scripts(code);
+
+
+    $el.text("");
+    $el.append($container);
+    $container.addClass("sb2");
+
+    for (var i=0; i<scripts.length; i++) {
+        var $script = scratchblocks2.render_stack(scripts[i]).addClass("script");
+        $container.append($script);
+    }
+
+   //  angular.element(element).ready(function () {
+			// scratchblocks2.parse('pre.scratchblocks');
+   //  });
 
 	};
 
 	return {
 		restrict: 'AE',
-		template: '<pre class="scratchblocks">{{code}}</pre>',
+		template: '',
 		link: link,
 		scope: {
 			code: "=code"
