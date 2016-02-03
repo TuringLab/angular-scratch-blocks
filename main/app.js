7b77206590462aa78e85b329271420707a533c1a
Henry Miskin <henry.miskin@talktalk.net>
2016-02-03 10:14:56 +0000

    Changed angular scratchblocks to update when the code is updated

--- main/app.js
+++ main/app.js
@@ -2,38 +2,38 @@
 
 angular.module('angularScratchblocks',[]).directive('scratchblock',function(){
 
-	function link(scope, element, attrs){
+  function link(scope, element, attrs){
 
-    var $el = $(element);
-    var $container = $('<div>');
+    scope.$watch(function(){ return scope.code },function(code){
 
-    var code = scope.code.replace(/<br>\s?|\n|\r\n|\r/ig, '\n');
-    var scripts = scratchblocks2.parse_scripts(code);
+      var $el = $(element);
+      var $container = $('<div>');
+
+      code = code.replace(/<br>\s?|\n|\r\n|\r/ig, '\n');
+      var scripts = scratchblocks2.parse_scripts(code);
 
 
-    $el.text("");
-    $el.append($container);
-    $container.addClass("sb2");
+      $el.text("");
+      $el.append($container);
+      $container.addClass("sb2");
 
-    for (var i=0; i<scripts.length; i++) {
-        var $script = scratchblocks2.render_stack(scripts[i]).addClass("script");
-        $container.append($script);
-    }
+      for (var i=0; i<scripts.length; i++) {
+          var $script = scratchblocks2.render_stack(scripts[i]).addClass("script");
+          $container.append($script);
+      }
+      
+    });
 
-   //  angular.element(element).ready(function () {
-			// scratchblocks2.parse('pre.scratchblocks');
-   //  });
+  };
 
-	};
-
-	return {
-		restrict: 'AE',
-		template: '',
-		link: link,
-		scope: {
-			code: "=code"
-		}
-	};
+  return {
+      restrict: 'AE',
+      template: '',
+      link: link,
+      scope: {
+          code: "=code"
+      }
+  };
 
 });
 
